f79c4098763e3fe81cd23cc4d10f91211cbff63e
mcremer-able <marc.cremer@able-group.de>
2023-01-21 17:31:30 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
+<script setup lang="ts">
+const { data } = await useFetch<{ id: number }>(
+  `https://jsonplaceholder.typicode.com/todos/1`
+);
+console.log(data.value);
+</script>
